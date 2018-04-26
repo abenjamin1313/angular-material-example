8ede49276c2cfa9c6f98b594625400db0db9af97
Adam Victor <abenjamin1313@gmail.com>
2018-04-26 12:32:44 -0400

    Add custom error handling.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, ErrorHandler } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
+
+import { XyzErrorHandlerService } from './error-handler.service';
 
 @NgModule({
   declarations: [
@@ -18,7 +20,9 @@ import { MaterialModule } from './material.module';
     MaterialModule,
     FormsModule
   ],
-  providers: [],
+  providers: [
+    { provide: ErrorHandler, useClass: XyzErrorHandlerService}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
